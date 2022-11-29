69e1cbce9f76053abe34d5eee9619fde97d6dd1f
KimBomnim <108743218+KimBomnim@users.noreply.github.com>
2022-11-29 10:03:29 +0900

    11-28
    시네 머신 구현(각도는 약간 이상함)

---
+++ Assets/Scripts/TargetMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetMove : MonoBehaviour
+{
+
+    public Transform target;
+
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+        target.position = transform.position;
+    }
+}
